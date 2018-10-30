1b280bc9f4c425e881b24d2125e4274713123721
Nancy <butlern0513@gmail.com>
2018-10-30 13:57:02 -0700

    added traffic light bulb on/off events

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 (function() {
   'use strict';
+document.addEventListener("DOMContentLoaded", function(){
+  var stopButtonEl = document.querySelector("#stopButton");
+  var slowButtonEl = document.querySelector("#slowButton");
+  var goButtonEl = document.querySelector("#goButton");
 
-  // YOUR CODE HERE
+  stopButtonEl.addEventListener("click", function addStop(){
+    document.querySelector("#stopLight").classList.toggle("stop")
+  });
+  slowButtonEl.addEventListener("click", function addSlow(){
+    document.querySelector("#slowLight").classList.toggle("slow")
+  });
+  goButtonEl.addEventListener("click", function addGo(){
+    document.querySelector("#goLight").classList.toggle("go")
+  });
+
+  var buttonArr = document.querySelectorAll(".button");
+
+  for(var i = 0; i < buttonArr.length; i++){
+    buttonArr[i].addEventListener("mouseenter", function(){
+    console.log(`Entered ${event.target.textContent} button`)
+    })
+    buttonArr[i].addEventListener("mouseleave", function(){
+      console.log(`Left ${event.target.textContent} button`)
+    })
+  };
+  document.querySelector("#controls").addEventListener("click", function(){
+    var action = event.target.innerText.toLowerCase();
+    if(document.querySelector(`#${action}Light`).classList.contains(action)){
+      console.log(`${action} bulb on`)
+    }
+    else{
+      console.log(`${action} bulb off`)
+    }
+  })
+});
 })();
